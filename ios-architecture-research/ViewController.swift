91cce883be2f0bc0312dd92b0da3feee5ad1e0bd
ushisantoasobu <babblemann.shunsee@gmail.com>
2019-03-19 19:32:53 +0900

    Initial Commit

---
+++ ios-architecture-research/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-architecture-research
+//
+//  Created by shunsuke.sato on 2019/03/19.
+//  Copyright © 2019 shunsuke.sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ios-architecture-researchTests/ios_architecture_researchTests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_architecture_researchTests.swift
+//  ios-architecture-researchTests
+//
+//  Created by shunsuke.sato on 2019/03/19.
+//  Copyright © 2019 shunsuke.sato. All rights reserved.
+//
+
+import XCTest
+@testable import ios_architecture_research
+
+class ios_architecture_researchTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
